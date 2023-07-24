7bfb407ee25378d0c62910f59f992795ff2e53ff
Filomn Abrha <filmonabrha2020@gmail.com>
2023-07-24 22:23:55 +0300

    Add a logo for the restaurant

--- src/home.js
+++ src/home.js
@@ -1,23 +1,27 @@
+import odin from "./odin.svg"
+
 export default function home() {
     const home = document.createElement("div")
     const homeText = document.createElement("h1")
     const name = document.createElement("h1")
     const container = document.createElement("div")
+    const logo = document.createElement("img")
 
     homeText.textContent = "The Best Restaurant In The Whole Hawzen To Eat And Hangout"
     homeText.classList.add('home-text')
     
     name.textContent = "ODIN Restaurant"
     name.classList.add("name")
-    const logo = new Image()
-    logo.src = "./odin.svg"
+    logo.src = odin
+    logo.classList.add("logo")
+    
+    container.appendChild(logo)
+    container.appendChild(name)
 
     container.classList.add("container")
     home.classList.add("home")
 
-    container.appendChild(logo)
-    container.appendChild(name)
-
+   
     home.appendChild(container)
     home.appendChild(homeText)
     return home
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './style.css'
 import home from "./home.js"
 import background from "./restaurant.jpg"
-import logo from "./odin.svg"
 
 
 const content = document.querySelector("#content")
