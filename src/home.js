5082b90f13763d75c81a059261e3979050293329
Filomn Abrha <filmonabrha2020@gmail.com>
2023-07-25 01:24:01 +0300

    Create buttons to navigate through the website

--- src/home.js
+++ src/home.js
@@ -6,24 +6,29 @@ export default function home() {
     const name = document.createElement("h1")
     const container = document.createElement("div")
     const logo = document.createElement("img")
+    const motto = document.createElement("p")
+    const description = document.createElement("p")
 
-    homeText.textContent = "The Best Restaurant In The Whole Hawzen To Eat And Hangout"
-    homeText.classList.add('home-text')
-    
     name.textContent = "ODIN Restaurant"
     name.classList.add("name")
     logo.src = odin
     logo.classList.add("logo")
-    
     container.appendChild(logo)
     container.appendChild(name)
-
     container.classList.add("container")
-    home.classList.add("home")
 
-   
+    motto.textContent = "Come! Sit! Relax & Eat!"
+    motto.classList.add("motto")
+
+    homeText.textContent = "The Best Restaurant In The Whole Hawzen To Eat And Hangout"
+    homeText.classList.add('home-text')
+    home.classList.add("home")
+    // description.textContent = ""
+
     home.appendChild(container)
+    home.appendChild(motto)
     home.appendChild(homeText)
+
     return home
     
 }
--- src/index.js
+++ src/index.js
@@ -4,4 +4,24 @@ import background from "./restaurant.jpg"
 
 
 const content = document.querySelector("#content")
+const homeBtn = document.createElement("button")
+const aboutBtn = document.createElement("button")
+const menuBtn = document.createElement("button")
+const orderBtn = document.createElement("button")
+const nav = document.createElement("div")
+
+nav.classList.add("nav")
+
+homeBtn.textContent = "Home"
+menuBtn.textContent = "Menu"
+aboutBtn.textContent = "About Us"
+orderBtn.textContent = "Delivery"
+orderBtn.classList.add("order")
+
+
+nav.appendChild(homeBtn)
+nav.appendChild(menuBtn)
+nav.appendChild(aboutBtn)
+nav.appendChild(orderBtn)
+content.appendChild(nav)
 content.appendChild(home())
