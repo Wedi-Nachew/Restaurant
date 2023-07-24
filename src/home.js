15ef69179f8f3b322be4b9ae55ae678350ed24b8
Filomn Abrha <filmonabrha2020@gmail.com>
2023-07-24 18:24:48 +0300

    Change variable names to a more descriptive names

--- src/home.js
+++ src/home.js
@@ -1,19 +1,25 @@
 export default function home() {
-    const div = document.createElement("div")
+    const home = document.createElement("div")
     const homeText = document.createElement("h1")
     const name = document.createElement("h1")
+    const container = document.createElement("div")
 
     homeText.textContent = "The Best Restaurant In The Whole Hawzen To Eat And Hangout"
     homeText.classList.add('home-text')
+    
     name.textContent = "ODIN Restaurant"
     name.classList.add("name")
+    const logo = new Image()
+    logo.src = "./odin.svg"
 
+    container.classList.add("container")
+    home.classList.add("home")
 
+    container.appendChild(logo)
+    container.appendChild(name)
 
-    div.classList.add("div")
-
-    div.appendChild(name)
-    div.appendChild(homeText)
-    return div
+    home.appendChild(container)
+    home.appendChild(homeText)
+    return home
     
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css'
 import home from "./home.js"
 import background from "./restaurant.jpg"
+import logo from "./odin.svg"
 
 
 const content = document.querySelector("#content")
