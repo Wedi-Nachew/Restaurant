f1fb44ecf65229bffb2b85b5e265396868c988ed
Filomn Abrha <filmonabrha2020@gmail.com>
2023-07-24 17:49:25 +0300

    Create & customize the basic home page

--- src/home.js
+++ src/home.js
@@ -1,9 +1,19 @@
 export default function home() {
+    const div = document.createElement("div")
     const homeText = document.createElement("h1")
+    const name = document.createElement("h1")
 
-    homeText.textContent = "The Best Restaurant In The Whole Town Of Hawzen To Eat And Hangout"
-    homeText.classList.add('home')
-    
-    return homeText
+    homeText.textContent = "The Best Restaurant In The Whole Hawzen To Eat And Hangout"
+    homeText.classList.add('home-text')
+    name.textContent = "ODIN Restaurant"
+    name.classList.add("name")
+
+
+
+    div.classList.add("div")
+
+    div.appendChild(name)
+    div.appendChild(homeText)
+    return div
     
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.css'
 import home from "./home.js"
+import background from "./restaurant.jpg"
 
 
 const content = document.querySelector("#content")
