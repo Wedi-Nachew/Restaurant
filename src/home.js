c11c272a41aed682fc905fd305b7f63340efb77d
Filomn Abrha <filmonabrha2020@gmail.com>
2023-07-27 09:56:10 +0300

    Move the logo & name to the nav and make it absolutely positioned

--- src/home.js
+++ src/home.js
@@ -1,31 +1,20 @@
-import odin from "./odin.svg"
+
 
 export default function home() {
     const home = document.createElement("div")
     const homeText = document.createElement("h1")
-    const name = document.createElement("h1")
-    const container = document.createElement("div")
-    const logo = document.createElement("img")
     const motto = document.createElement("p")
     const description = document.createElement("p")
 
-    name.textContent = "ODIN Restaurant"
-    name.classList.add("name")
-    logo.src = odin
-    logo.classList.add("logo")
-    container.appendChild(logo)
-    container.appendChild(name)
-    container.classList.add("container")
-
+    
     motto.textContent = "Come! Sit! Relax & Eat!"
     motto.classList.add("motto")
-
     homeText.textContent = "The Best Restaurant In The Whole Hawzen To Eat And Hangout"
     homeText.classList.add('home-text')
     home.classList.add("home")
     // description.textContent = ""
 
-    home.appendChild(container)
+    // home.appendChild(container)
     home.appendChild(motto)
     home.appendChild(homeText)
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import './style.css'
 import home from "./home.js"
 import aboutUs from "./about.js"
 import menu from "./menu.js"
+import odin from "./odin.svg"
+// import container  from 'webpack'
 
 
 const content = document.querySelector("#content")
@@ -10,9 +12,16 @@ const aboutBtn = document.createElement("button")
 const menuBtn = document.createElement("button")
 const orderBtn = document.createElement("button")
 const nav = document.createElement("div")
+const logo = document.createElement("img")
+const name = document.createElement("h1")
+const container = document.createElement("div")
 
 nav.classList.add("nav")
 
+name.textContent = "ODIN Restaurant"
+name.classList.add("name")
+logo.src = odin
+logo.classList.add("logo")
 homeBtn.textContent = "Home"
 menuBtn.textContent = "Menu"
 aboutBtn.textContent = "About Us"
@@ -36,9 +45,13 @@ nav.addEventListener("click", (event)=>{
 
 
 document.addEventListener("DOMContentLoaded", ()=>{
-    content.appendChild(menu())
+    content.appendChild(home())
 })
 
+container.appendChild(logo)
+container.appendChild(name)
+container.classList.add("container")
+nav.appendChild(container)
 nav.appendChild(homeBtn)
 nav.appendChild(menuBtn)
 nav.appendChild(aboutBtn)
